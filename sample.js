124a0e72e0bab789b2772d90da5a56237424f3e1
DONA PAUL <donapaul901@gmail.com>
2025-12-21 19:50:41 +0530

    javascript

---
+++ sample.js
@@ -0,0 +1,17 @@
+// variable keywords
+
+// var
+// let
+// const
+
+var name = "python"
+name =  "django"
+console.log("name")
+
+let subject = "maths"
+subject = "physics"
+console.log("subject")
+
+const age = 26
+age = 30
+console.log("age")
