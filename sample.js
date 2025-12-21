6c6cc4aba8a3209cc9a1720912bcdeb512715abb
DONA PAUL <donapaul901@gmail.com>
2025-12-21 19:51:29 +0530

    cmt

--- sample.js
+++ sample.js
@@ -8,10 +8,10 @@ var name = "python"
 name =  "django"
 console.log("name")
 
-let subject = "maths"
-subject = "physics"
-console.log("subject")
+// let subject = "maths"
+// subject = "physics"
+// console.log("subject")
 
-const age = 26
-age = 30
-console.log("age")
+// const age = 26
+// age = 30
+// console.log("age")
