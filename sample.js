cdd0c748864b0c9e2a90a32e9c5421a5dde21e5b
DONA PAUL <donapaul901@gmail.com>
2025-12-22 09:36:44 +0530

    cmt

--- sample.js
+++ sample.js
@@ -16,6 +16,20 @@
 // age = 30
 // console.log("age")
 
-var name = "hello"
+// var name = "hello";
 
-console.log(name)
+// console.log(name)
+
+// datatypes
+// =============
+
+// 1.Number
+
+num = 10;
+
+console.log(typeof num)
+
+// 2.Bigint
+
+// 3.String
+
