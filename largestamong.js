8897139532ddf7ffa54cc16c2696a06970c07c8b
DONA PAUL <donapaul901@gmail.com>
2025-12-23 09:31:49 +0530

    largest among three

--- largestamong.js
+++ largestamong.js
@@ -12,6 +12,6 @@ if(num1 > num2 && num1 > num3){
 else if(num2 > num1 && num2 > num3){
     console.log(num2,"is largest")
 }
-// else{
-//     console.log(num3,"is largest")
-// }
+else{
+    console.log(num3,"is largest")
+}
