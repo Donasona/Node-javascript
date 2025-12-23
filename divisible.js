fbbe79e97f0756b6911f31a2130946d159074f59
DONA PAUL <donapaul901@gmail.com>
2025-12-23 09:51:16 +0530

    divisible

--- divisible.js
+++ divisible.js
@@ -1,13 +1,25 @@
 //check a number is divisible by both 3 and 5 or not
 
-num = 25
+num = 40
 
 if(num%3==0 && num%5==0){
     console.log(num,"is divisible by 3 and 5")
 }
 else{
-    console.log(num,"is not divisible by 3 and 5")
+    console.log(num,"is not divisible by both 3 and 5")
 }
 
 //divisible by 3 not by 5
 
+if(num%3==0 && num%5==0){
+    console.log(num,"is divisible by 3 and 5")
+}
+else if(num %3==0 && num%5!=0){
+    console.log(num,"divisible by 3 not by 5")
+}
+else if(num % 5 ==0 && num % 3!=0){
+    console.log(num,"divisible by 5 not by 3")
+}
+else{
+    console.log(num,"not by both 3 and 5")
+}
