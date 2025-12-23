bb3b329c68669b42460576b7861f50b7de5d2bba
DONA PAUL <donapaul901@gmail.com>
2025-12-23 19:55:48 +0530

    odd even

---
+++ oddeven.js
@@ -0,0 +1,13 @@
+num=4
+if(num % 2==0){
+    console.log(num,"even")
+}
+else{
+    console.log(num,"odd")
+}
+
+// using ternary operator
+// condition ? True : False
+
+result = num % 2 == 0?"even":"odd"
+console.log(result)
