7ead03dfa454d2dece763548cb069e44c37afc71
DONA PAUL <donapaul901@gmail.com>
2025-12-24 10:32:25 +0530

    loop questions

--- for loop.js
+++ for loop.js
@@ -1,7 +1,54 @@
+// increment
+
 for(i = 0;i<=5;i++){
     console.log(i)
 }
 
+// decrement
+
 for(i=10;i>=1;i--){
-    console.log(i)
+    console.log(i);
 }
+
+// even number
+
+for(i=2;i<20;i+=2){
+    console.log(i);
+}
+
+//count how many numbers are divsible by 3 in 1 and 5
+
+// let count=0
+// for(i=1;i<=50;i++){
+
+//     if(i%3==0){
+//         count+=1;
+//     }
+// }
+// console.log(count)
+
+// count the total number of characters in the string
+
+// let text = "javascript"
+
+// let count = 0
+// for(i=0;i<text.length;i++){
+//    count +=1
+// }
+// console.log(count)
+
+// return the count of a specific charater fron a string
+
+// let text = "mississippi"
+// let target = "s"
+// count=0
+// for(i=0;i<text.length;i++){
+//     if (text[i]==target){
+//         count ++
+//     }
+// }
+// console.log(count)
+
+// print the elements at the even index positions
+
+let text = "javascript"
