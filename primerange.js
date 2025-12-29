9f009ccdee6eba18cfc0bb433284dd32433c1a79
DONA PAUL <donapaul901@gmail.com>
2025-12-29 10:16:40 +0530

    total prime

--- primerange.js
+++ primerange.js
@@ -1,19 +1,64 @@
 // check and print all the prime num in a given range
 
+// let start = 1
+// let stop = 50
+// for(let num = start;num <= stop;num++){
+//     if(num>1){
+//         isPrime = true;
+//         for(let i = 2;i<num ; i++){
+//             if(num%i==0){
+//                 isPrime = false;
+//                 break;
+//             }
+//         }
+//         if ( isPrime == true){
+//             console.log(num)
+//         }
+//     }
+          
+// }
+
+// identify the total num of prime num in a given range
+// identify the largest prime num in the geven range
+
+// 1.
+// let start = 1
+// let stop = 50
+// let count = 0;
+//  for(let num = start;num <= stop;num++){
+//     if(num>1){
+//     let isPrime=true;
+//     for(i=2;i<num;i++){
+//         if(num%i==0){
+//             isPrime=false;
+//             break;
+//         }
+//     }
+//     if(isPrime == true){
+//         count++;
+//     }
+//     }
+
+//  }
+//  console.log("total prime num:",count)
+
+//  2.
 let start = 1
 let stop = 50
-for(let num = start;num <= stop;num++){
+let count = 0;
+ for(let num = start;num <= stop;num++){
     if(num>1){
-        isPrime = true;
-        for(let i = 2;i<num ; i++){
-            if(num%i==0){
-                isPrime = false;
-                break;
-            }
-        }
-        if ( isPrime == true){
-            console.log(num)
+    let isPrime=true;
+    for(i=2;i<num;i++){
+        if(num%i==0){
+            isPrime=false;
+            break;
         }
     }
-          
-}
+    if(isPrime == true){
+        count++;
+    }
+    }
+
+ }
+ console.log("total prime num:",count)
