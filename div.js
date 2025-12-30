ea56e763d5410211ca4e51f20ed83e6082f134ad
DONA PAUL <donapaul901@gmail.com>
2025-12-30 09:39:25 +0530

    sum of even

--- div.js
+++ div.js
@@ -1,9 +1,25 @@
 //print the num divisible by both 3 and 5 from 13 to 105
 
-let i = 13
-while(i<=105){
-    if(i%3==0 && i%5==0){
-        console.log(i)
-    }
-    i++;
-}
+// let i = 13
+// while(i<=105){
+//     if(i%3==0 && i%5==0){
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+//find the sum of even num
+
+// let i = 1;
+// let sum =0;
+
+// while(i<=3){
+
+//     if(i%2==0){
+//         sum+=1
+//     }
+//     i++;
+// }
+// console.log(sum);
+
+
