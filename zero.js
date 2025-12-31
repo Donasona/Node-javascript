58dabf8ccd74c4e9e063dbf4614792b2ccf37272
DONA PAUL <donapaul901@gmail.com>
2025-12-31 09:50:34 +0530

    zeros

---
+++ zero.js
@@ -0,0 +1,12 @@
+// move all zero from the array to the end
+num = [1,3,0,7,6,9,0]
+let result =[]
+for(let i=0;i<num.length;i++){
+    if(num[i]!=0){
+        result.push(num[i])
+    }
+}
+while(result.length<num.length){
+    result.push(0)
+}
+console.log(result)
