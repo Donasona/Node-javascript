753b3f916536386c1008d236cde6db6f2b18b158
DONA PAUL <donapaul901@gmail.com>
2026-01-02 10:29:41 +0530

    count

--- sample_object.js
+++ sample_object.js
@@ -1,6 +1,11 @@
 // how many times each word appears
 
 let text = "javascript is easy and javascript is powerful"
-
+count = new Map()
 let words = text.split(" ")
 console.log(words)
+
+for(let i of words){
+    count.set(i , (count.get(i) || 0) + 1);
+}
+console.log(count)
