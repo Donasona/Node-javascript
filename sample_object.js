8cad009be8f5d1ea56d2fdce6191ec572bdaeec4
DONA PAUL <donapaul901@gmail.com>
2026-01-04 22:30:04 +0530

    cmt

--- sample_object.js
+++ sample_object.js
@@ -1,9 +1,9 @@
 // how many times each word appears
 
-let text = "javascript is easy and javascript is powerful"
-count = new Map()
-let words = text.split(" ")
-console.log(words)
+// let text = "javascript is easy and javascript is powerful"
+// count = new Map()
+// let words = text.split(" ")
+// console.log(words)
 
 for(let i of words){
     count.set(i , (count.get(i) || 0) + 1);
