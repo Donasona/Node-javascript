702d178b0ea74d1b67a3460c6346d08f9c883633
DONA PAUL <donapaul901@gmail.com>
2026-01-05 11:45:37 +0530

    cmt

--- largestamong.js
+++ largestamong.js
@@ -4,15 +4,15 @@ var num1 = 3
 
 var num2 = 5
 
-// var num3 = 7
+var num3 = 7
 
-// if(num1 > num2 && num1 > num3){
-//     console.log(num1,"is largest")
-// }
-// else if(num2 > num1 && num2 > num3){
-//     console.log(num2,"is largest")
-// }
-// else
-//     {
-//     console.log(num3,"is largest")
-// }
+if(num1 > num2 && num1 > num3){
+    console.log(num1,"is largest")
+}
+else if(num2 > num1 && num2 > num3){
+    console.log(num2,"is largest")
+}
+else
+    {
+    console.log(num3,"is largest")
+}
